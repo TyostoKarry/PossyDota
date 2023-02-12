b3e014643d0d0f0d10498ad826082e1a07614c22
TyostoKarry <samuritala@hotmail.com>
2023-02-12 04:19:06 +0200

    Set activity

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const { Interaction } = require("chart.js");
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
+  client.user.setActivity("!help");
 });
 
 client.on("messageCreate", (message) => {
